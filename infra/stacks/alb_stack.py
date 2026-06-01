e7c8330ce5ba079f33440785c906f015ed60d294
Paul HAMON <paulhamon281@gmail.com>
2026-06-01 14:01:06 +0200

    added from_security_group_id in alb stack

--- infra/stacks/alb_stack.py
+++ infra/stacks/alb_stack.py
@@ -10,20 +10,25 @@ class AlbStackPrimary(Stack):
     def __init__(self, scope: Construct, construct_id: str, vpc_stack, sg_stack, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # 1. Création de l'ALB public en ciblant les variables des autres stacks
+        # 1. On importe et convertit le SG de ton collègue (alb_sg) pour qu'il soit compatible
+        sg_alb_l2 = ec2.SecurityGroup.from_security_group_id(
+            self, "ImportedSgAlbPrimary",
+            security_group_id=sg_stack.alb_sg.ref
+        )
+
+        # 2. Création de l'ALB public avec le SG converti
         self.load_balancer = elbv2.ApplicationLoadBalancer(
             self, "AlbPrimary",
             vpc=vpc_stack.vpc_prod,
             internet_facing=True,
-            security_group=sg_stack.sg_alb,
-            # On place l'ALB spécifiquement dans les subnets publics nommés
+            security_group=sg_alb_l2, # <-- Utilisation du SG converti
             vpc_subnets=ec2.SubnetSelection(subnets=[
                 vpc_stack.sub_public_1_prod, 
                 vpc_stack.sub_public_2_prod
             ])
         )
 
-        # 2. Création du Target Group (Cible) pour l'Auto Scaling Group
+        # 3. Création du Target Group
         self.target_group = elbv2.ApplicationTargetGroup(
             self, "TargetGroupPrimary",
             vpc=vpc_stack.vpc_prod,
@@ -37,7 +42,7 @@ class AlbStackPrimary(Stack):
             )
         )
 
-        # 3. Création du Listener
+        # 4. Création du Listener
         self.listener = self.load_balancer.add_listener(
             "ListenerPrimary",
             port=80,
@@ -52,19 +57,25 @@ class AlbStackSecondary(Stack):
     def __init__(self, scope: Construct, construct_id: str, vpc_stack, sg_stack, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # 1. Création de l'ALB de secours
+        # 1. On importe et convertit le SG de secours
+        sg_alb_l2_secondary = ec2.SecurityGroup.from_security_group_id(
+            self, "ImportedSgAlbSecondary",
+            security_group_id=sg_stack.alb_sg.ref
+        )
+
+        # 2. Création de l'ALB de secours
         self.load_balancer = elbv2.ApplicationLoadBalancer(
             self, "AlbSecondary",
             vpc=vpc_stack.vpc_backup,
             internet_facing=True,
-            security_group=sg_stack.sg_alb,
+            security_group=sg_alb_l2_secondary, # <-- Utilisation du SG converti
             vpc_subnets=ec2.SubnetSelection(subnets=[
                 vpc_stack.sub_public_1_backup, 
                 vpc_stack.sub_public_2_backup
             ])
         )
 
-        # 2. Création du Target Group de secours
+        # 3. Création du Target Group de secours
         self.target_group = elbv2.ApplicationTargetGroup(
             self, "TargetGroupSecondary",
             vpc=vpc_stack.vpc_backup,
@@ -78,7 +89,7 @@ class AlbStackSecondary(Stack):
             )
         )
 
-        # 3. Création du Listener de secours
+        # 4. Création du Listener de secours
         self.listener = self.load_balancer.add_listener(
             "ListenerSecondary",
             port=80,
