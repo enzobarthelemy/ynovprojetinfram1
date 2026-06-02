51af0f1cdfbca443f615528ca5b04e8bc042bc68
Thomas Marcilly <thomasmarcilly@yahoo.com>
2026-06-02 14:04:49 +0200

    cutover: retrait suffixes -nested (vrais noms de ressources)

--- infra/stacks/nested/alb.py
+++ infra/stacks/nested/alb.py
@@ -15,7 +15,7 @@ class AlbNested(NestedStack):
         super().__init__(scope, construct_id, **kwargs)
 
         alb = elbv2.CfnLoadBalancer(self, "Alb",
-            name=f"wp-alb-{name}-nested",
+            name=f"wp-alb-{name}",
             scheme="internet-facing",
             type="application",
             security_groups=[alb_sg_id],
@@ -23,7 +23,7 @@ class AlbNested(NestedStack):
         )
 
         tg = elbv2.CfnTargetGroup(self, "TargetGroup",
-            name=f"wp-tg-{name}-nested",
+            name=f"wp-tg-{name}",
             port=80,
             protocol="HTTP",
             vpc_id=vpc_id,
